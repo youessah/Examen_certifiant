9bcdde26cb678e91996206c7a3875b8c248c01ef
youessah <youessahaudrey@gmail.com>
2026-03-08 23:07:47 +0100

    essai sonaqurbe

---
+++ Examen_certifiant/src/test/java/com/ecomove/ecoMove/EcoMoveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ecomove.ecoMove;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EcoMoveApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
